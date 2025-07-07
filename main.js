4739ed81ad544f0f5622350a2b1beef36fb68aca
Supergamer45550 <gamespcandmobile93@gmail.com>
2025-07-07 18:25:23 +0200

    added index.html for the webapp

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ function createWindow () {
   // mainWindow.webContents.openDevTools()
 }
 
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -41,3 +42,4 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
